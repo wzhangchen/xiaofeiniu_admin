02be6c403e6d10c7574e5d7ce32db6bfd0b7a8e8
zhangchenguang <zhangchenguang@163.com>
2019-02-01 09:18:16 +0800

    vue

---
+++ src/components/table.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="xfn-table-info">
+    <el-card shadow="hover">
+      <div class="table"></div>
+      <el-button size="mini" type="success" plain>详情</el-button>
+      <el-button size="mini" type="danget" plain>修改</el-button>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props:["data"]
+}
+</script>
+<style lang="scss">
+  
+</style>
